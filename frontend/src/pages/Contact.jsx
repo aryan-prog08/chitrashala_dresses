29d507e65e8639841ecc2ad480cb41c59cdfe52c
Aryan <gupta.aryan0807@gmail.com>
2025-08-13 09:44:28 +0530

    changed details on contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
           <p className='text-gray-500'>A46 Shyam Park Extension <br /> Sahibabad, Ghaziabad</p>
-          <p className='text-gray-500'>Tel: +91 9971169639 <br /> Email: preetig0204@gmail.com</p>
+          <p className='text-gray-500'>Tel: +91 7428621606 <br /> Email: gupta.aryan0807@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Chitrashala</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 cursor-pointer'>Explore Jobs</button>
